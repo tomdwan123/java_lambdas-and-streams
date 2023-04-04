814c9cd300a2a2e69d7274a310cd45dc6a317dee
phuc.letan <phuc.letan@messagemedia.com>
2023-04-04 14:19:20 +0700

    ✨ Init predicate and biPredicate

---
+++ src/com/phucdevs/lambdas/predicate_bipredicate/FIFromAPI.java
@@ -0,0 +1,16 @@
+package com.phucdevs.lambdas.predicate_bipredicate;
+
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+public class FIFromAPI {
+
+    public static void main(String[] args) {
+
+        Predicate<String> predicate = s -> s.contains("city");
+        System.out.println(predicate.test("Newyork city")); // true
+
+        BiPredicate<String, Integer> biPredicate = (str, len) -> str.length() == len;
+        System.out.println(biPredicate.test("Newyork city", 8)); // false (length is 12)
+    }
+}
